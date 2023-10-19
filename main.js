2adfffd6c40742fd612b444c53cd56bd3a4689f8
HARRMOS <108294489+HARRMOS@users.noreply.github.com>
2023-10-19 09:32:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+function menu()
+{
+    document.getElementById("liste").style.display = "block";
+}
+
+function html()
+{
+    document.getElementById("menu").style.display = "block";
+    
+}
+function tp()
+{
+    document.getElementById("tp1").style.display = "block";
+    document.getElementById("tp2").style.display = "block";
+    document.getElementById("tp3").style.display = "block";
+    document.getElementById("tp4").style.display = "block";
+    document.getElementById("tp5").style.display = "block";
+    document.getElementById("tp6").style.display = "block";
+
+    document.getElementById("m1").style.display = "none";
+    document.getElementById("m2").style.display = "none";
+    document.getElementById("m3").style.display = "none";
+    document.getElementById("m4").style.display = "none";
+    document.getElementById("m5").style.display = "none";
+   
+}
+function mission()
+{
+    document.getElementById("tp1").style.display = "none";
+    document.getElementById("tp2").style.display = "none";
+    document.getElementById("tp3").style.display = "none";
+    document.getElementById("tp4").style.display = "none";
+    document.getElementById("tp5").style.display = "none";
+    document.getElementById("tp6").style.display = "none";
+
+    document.getElementById("m1").style.display = "block";
+    document.getElementById("m2").style.display = "block";
+    document.getElementById("m3").style.display = "block";
+    document.getElementById("m4").style.display = "block";
+    document.getElementById("m5").style.display = "block";
+   
+}
+
+function certif()
+{
+    document.getElementById("c1").style.display = "block";
+    
+    
+}
