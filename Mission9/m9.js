69a59f76fa25bfd16fd7431e00654c8213fc140d
HARRMOS <108294489+HARRMOS@users.noreply.github.com>
2023-11-18 14:21:43 +0100

    Add files via upload

---
+++ Mission9/m9.js
@@ -0,0 +1,35 @@
+function calculer()
+{
+    document.getElementById("description");
+    document.getElementById("quantite");
+    document.write(description).getElementById;
+
+
+
+
+}
+
+
+
+function Ajout_ligne()
+{
+
+        // create a new div element
+        var node = document.getElementById("fac");
+    
+        var s = node.cloneNode(true);
+        
+        document.getElementById("fac").appendChild(s);
+      
+        
+      }
+
+function remplire()
+{
+
+}
+
+function Annuler()
+{
+    window.location.reload();
+}
