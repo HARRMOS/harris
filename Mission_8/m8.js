64f92c1cf6509ff5299922aaaeef26efb4268514
HARRMOS <108294489+HARRMOS@users.noreply.github.com>
2023-11-29 17:10:21 +0100

    Update m8.js

--- Mission_8/m8.js
+++ Mission_8/m8.js
@@ -205,9 +205,23 @@ document.body.style.background = color;
 }
 
 
-function fonction20()
-{
-    
+function bonus(){
+    var x = document.getElementById("i1").value;
+    var a = document.getElementById("i2").value;
+    var taille = x.length;
+    var maj = x.toUpperCase();
+
+   
+    if ( x.includes(a))
+    {
+        alert(maj+' Constient '+ taille+" caractere " );
+    }
+    else
+    {
+        alert("La chaine "+x  +" ne contient pas " + a);
+    }
+
+
 }
 
 
